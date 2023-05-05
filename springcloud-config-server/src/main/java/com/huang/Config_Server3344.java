1b8adf9df35091d5f7be760593c53300da01f24a
香蕉木头人520 <96758259+huanghongtao520@users.noreply.github.com>
2023-05-05 11:03:45 +0800

    最后一次提交

---
+++ springcloud-config-server/src/main/java/com/huang/Config_Server3344.java
@@ -0,0 +1,13 @@
+package com.huang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer //开启Config
+public class Config_Server3344 {
+    public static void main(String[] args) {
+        SpringApplication.run(Config_Server3344.class,args);
+    }
+}
